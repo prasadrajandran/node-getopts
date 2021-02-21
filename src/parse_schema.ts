28413e14e71b27e1f297668c857fe2bf58db6420
Prasad Rajandran <prasadrajandran@gmail.com>
2021-02-20 22:19:50 -0500

    Update CLI schema parser
    - Use new SchemaError class
    - Behaviour of `minArgs` has been fixed for CLI commands. It can now be used to specify if a command is optional (0) or required (1)

--- src/parse_schema.ts
+++ src/parse_schema.ts
@@ -2,6 +2,7 @@ import { Schema } from './interfaces/schema';
 import { Config, OptConfigMap } from './interfaces/config';
 import { parseCmdSchema } from './parse_cmd_schema';
 import { parseOptSchema } from './parse_opt_schema';
+import { SchemaError } from './classes/errors';
 
 /**
  * Parse a schema.
@@ -20,45 +21,46 @@ export const parseSchema = (
   const maxArgs = schema.maxArgs ?? (expectsCmd ? 1 : Infinity);
 
   if (expectsCmd) {
-    if (Number.isFinite(minArgs) && minArgs !== 1) {
-      throw new Error(
-        `[SCHEMA] If a command is expected, the only valid value for ` +
-          `"minArgs" is 1. It should ideally be left undefined as it would ` +
-          `automatically be set to 1 in this case: ${minArgs}`,
+    if (Number.isFinite(minArgs) && minArgs !== 0 && minArgs !== 1) {
+      throw new SchemaError(
+        `If a command is expected (instead of args), the only valid values ` +
+          `for "minArgs" is 0 (command is optional) or 1 (command is required` +
+          `) If left undefined, it will be set to 1 by default. Got ` +
+          `"${minArgs}"`,
       );
     } else if (Number.isFinite(maxArgs) && maxArgs !== 1) {
-      throw new Error(
-        `[SCHEMA] If a command is expected, the only valid value for ` +
-          `"maxArgs" is 1. It should ideally be left undefined as it would ` +
-          `automatically be set to 1 in this case: ${maxArgs}`,
+      throw new SchemaError(
+        `If a command is expected (instead of args), the only valid value for` +
+          ` "maxArgs" is 1. It should ideally be left undefined as it would ` +
+          `automatically be set to 1. Got "${maxArgs}"`,
       );
     }
   }
 
   if (minArgs < 0) {
-    throw new Error(`[SCHEMA] "minArgs" cannot be less than 1: ${minArgs}`);
+    throw new SchemaError(`"minArgs" cannot be less than 1. Got "${minArgs}"`);
   } else if (!Number.isInteger(minArgs)) {
-    throw new Error(
-      `[SCHEMA] "minArgs" is expected to be a whole number: ${minArgs}`,
+    throw new SchemaError(
+      `"minArgs" is expected to be a whole number. Got "${minArgs}"`,
     );
   }
 
   if (minArgs > maxArgs) {
-    throw new Error(
-      `[SCHEMA] "minArgs" cannot be more than "maxArgs": ` +
-        `${minArgs}, ${maxArgs}`,
+    throw new SchemaError(
+      `"minArgs" cannot be more than "maxArgs": ${minArgs}, ${maxArgs}`,
     );
   } else if (!Number.isInteger(maxArgs) && maxArgs !== Infinity) {
-    throw new Error(
-      `"maxArgs" is expected to be a whole number or Infinity: ${maxArgs}`,
+    throw new SchemaError(
+      `"maxArgs" is expected to be a whole number or Infinity. Got ` +
+        `"${maxArgs}"`,
     );
   }
 
   const opts = parseOptSchema(schema.opts || []);
   for (const optName of opts.keys()) {
     if (alreadyDefinedOpts.has(optName)) {
-      throw new Error(
-        `[SCHEMA] Duplicate option found in this sequence: "${optName}"`,
+      throw new SchemaError(
+        `Duplicate option, "${optName}", found in this command sequence`,
       );
     }
   }
