5e7dfb85eb272e6d6ceb073860e253b3832fb1da
Prasad Rajandran <prasadrajandran@gmail.com>
2021-02-24 22:51:26 -0500

    Update parse.ts
    - [BREAKING] remove "config", "execPath", "module", and "input" from ParsedArgs. No good reason to provide these values
    - [BREAKING] Second argument for "parse" is now an object so that more properties can be easily added to it should the need arise
    - [BREAKING] If the user wishes to explicity set the argv property, there is no need to include the execPath or module in the array

--- src/interfaces/parsed_args.ts
+++ src/interfaces/parsed_args.ts
@@ -1,28 +1,9 @@
-import { Config } from './config';
 import { OptMap } from './opt_map';
 
 /**
  * Parsed CLI arguments.
  */
 export interface ParsedArgs {
-  /**
-   * Parsed CLI schema.
-   *
-   * Note: can be used to verify if the CLI schema was properly parsed.
-   */
-  config: Config;
-  /**
-   * Absolute pathname of the executable that started the Node.js process.
-   */
-  execPath: string;
-  /**
-   * Path to the JavaScript file being executed.
-   */
-  module: string;
-  /**
-   * CLI input.
-   */
-  input: string;
   /**
    * Array of parsed CLI commands.
    */
