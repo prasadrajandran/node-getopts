4a1b43794d481cd80ac602b6868a672e5c7fa52c
Prasad Rajandran <prasadrajandran@gmail.com>
2021-02-28 22:06:35 -0500

    Add sorting package.json as a step and make use of the "strip-comments-cli" package

--- prep_release.sh
+++ prep_release.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
-echo "1. cleaning dist dir..."
+echo "1. sort package.json"
+npx sort-package-json
+
+echo "2. cleaning dist dir..."
 rm -rf dist
 
-echo "2. cleaning docs dir..."
+echo "3. cleaning docs dir..."
 rm -rf docs
 
-echo "3. building..."
+echo "4. building..."
 npm run build
 
-echo "4. stripping comments from dist JS files..."
-node ./strip_comments.js
+echo "5. stripping comments from dist JS files..."
+shopt -s globstar # enable recursive globbing
+npx stripcomments ./dist/**/*.js --write --confirm-overwrite
 
-echo "5. building docs..."
+echo "6. building docs..."
 npm run build-docs
 
 # Remove the first two lines from README file. The title is duplicated for
 # some reason.
 tail -n +3 ./docs/README.md > ./docs/temp && mv ./docs/temp ./docs/README.md
 
-echo "6. linting..."
+echo "7. linting..."
 npm run lint
 
-echo "7. run prettier..."
+echo "8. run prettier..."
 npm run prettier-fix
 
-echo "8. testing..."
+echo "9. testing..."
 npm test
 
 package_version=`cat package.json | grep version`
@@ -33,12 +37,11 @@ package_version=${package_version/  \"version\"\: /} # remove `  "version": `
 package_version=${package_version/,/} # remove `,`
 
 echo
-echo
 echo "--- RELEASE PREP COMPLETE ---"
 echo
 echo "1. update the package version if necessary: $package_version"
-echo "2. do not forget to run \"npm install\" if the package version is updated"
+echo "2. do not forget to run \"npm install\" if the package version is updated so that the lockfile is updated too"
 echo "3. create PR to main (from development), merge PR, and create a GitHub release"
 echo "4. switch to the main branch locally and run \"git pull\""
 echo "5. to publish the package run: \"npm publish --access public\""
-echo "6. switch back to development and run \"git pull\""
+echo "6. switch back to development, merge main, and run \"git push\""
