f0727a2bdb0205955b979b3cc1771bc452ea7906
Prasad Rajandran <prasadrajandran@gmail.com>
2021-08-22 15:19:13 -0400

    Update smoke test

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,5 +1,5 @@
 const setArgv = require('./helpers/set_argv');
-const { getopts } = require('../d');
+const { getopts } = require('../dist');
 
 const resetArgv = () => setArgv('');
 
@@ -12,21 +12,32 @@ describe('smoke test', () => {
   });
 
   test('parses options/commands/arguments', () => {
-    setArgv('-n5 --verbose create -f -- somefile1 somefile2 -x');
+    setArgv('-n5 --verbose -T create -f -- somefile1 somefile2 -x');
 
     const { opts, cmds, args, errors } = getopts({
       opts: [
-        { name: '-n', arg: 'required', optArgFilter: (v) => parseInt(v, 10) },
-        { longName: '--verbose' },
+        {
+          name: '-n',
+          arg: { required: true, filter: (v) => parseInt(v, 10) },
+        },
+        { name: '--help' },
+        { name: ['-V', '--verbose'] },
+        { name: ['-S', '--sort', '-T', '--sort-by'] },
       ],
       cmds: [{ name: 'create', opts: [{ name: '-f' }] }],
     });
 
     expect(errors).toEqual([]);
 
-    expect(Array.from(opts.keys())).toEqual(['-n', '--verbose', '-f']);
+    expect(Array.from(opts.keys())).toEqual(['-n', '--verbose', '-T', '-f']);
     expect(opts.has('-n')).toBe(true);
+    expect(opts.has('--help')).toBe(false);
+    expect(opts.has('-V')).toBe(false);
     expect(opts.has('--verbose')).toBe(true);
+    expect(opts.has('-S')).toBe(false);
+    expect(opts.has('-T')).toBe(true);
+    expect(opts.has('--sort')).toBe(false);
+    expect(opts.has('--sort-by')).toBe(false);
     expect(opts.has('-f')).toBe(true);
 
     expect(opts.get('-n')).toEqual(5);
